5a1915c575515441f8338962952db6e355902111
zonesama <trannhatthanh31@gmail.com>
2019-10-21 20:37:38 +0700

    First Commit.

---
+++ src/primeList.java
@@ -0,0 +1,36 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class primeList {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int input = sc.nextInt();
+        ArrayList<Integer> primeList = new ArrayList<>();
+        int beginNumber = 2;
+        while(beginNumber< input){
+            if(isPrime(beginNumber)){
+                primeList.add(beginNumber);
+            }
+            beginNumber++;
+        }
+        System.out.println("Array of Prime Numbers that lower than "+input+" : ");
+        for(int j =0; j<primeList.size();j++){
+            System.out.println(primeList.get(j));
+        }
+    }
+
+    private static boolean isPrime(int in){
+        if(in<2){
+            return false;
+        }else{
+            for (int i = 2; i<= Math.sqrt(in);i++){
+                if(in % i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
